3e6c6ab45ba7833eb263f4054fd8fc76994e2c15
Carranza Fernandez Fidel Eduardo <zero.fidel26@gmail.com>
2022-10-25 19:40:54 -0500

    DesafioEntregable3-IntegrandoAPIS

---
+++ DesafioEntregable3/PrimeraEntrega-ProyectoFinal/Controllers/ProductoController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PrimeraEntrega_ProyectoFinal.Models;
+using PrimeraEntrega_ProyectoFinal.Repositories;
+
+namespace PrimeraEntrega_ProyectoFinal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+        ProductoRepository productoRepository;
+
+        public ProductoController()
+        {
+            productoRepository = new ProductoRepository();
+        }
+
+        [HttpGet("ObtenerProducto")]
+        public ActionResult ObtenerProducto([FromQuery] int IdUsuario)
+        {
+            var result = productoRepository.TraerProducto(IdUsuario);
+            return Ok(result);
+        }
+
+        [HttpGet("ObtenerProductos")]
+        public ActionResult ObtenerProductos()
+        {
+            var result = productoRepository.TraerProductos();
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public void CrearProducto([FromQuery] Producto producto)
+        {
+
+            productoRepository.CrearProducto(producto);
+        }
+
+        [HttpPut]
+        public void ModificarProducto([FromQuery] Producto producto)
+        {
+            productoRepository.ModificarProducto(producto);
+
+        }
+
+        [HttpDelete]
+        public void EliminarProducto([FromQuery] int idProducto)
+        {
+            productoRepository.EliminarProducto(idProducto);
+        }
+    }
+}
---
+++ DesafioEntregable3/PrimeraEntrega-ProyectoFinal/Repositories/ProductoVendidoRepository.cs
@@ -0,0 +1,181 @@
+﻿using PrimeraEntrega_ProyectoFinal.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PrimeraEntrega_ProyectoFinal.Repositories
+{
+    public class ProductoVendidoRepository
+    {
+        public List<ProductoVendido> TraerProductoVendido(int IdUsuario)
+        {
+            List<ProductoVendido> listaProductosVendidos = new List<ProductoVendido>();
+            ProductoRepository producto = new ProductoRepository();
+
+            List<Producto> ListaProducto = producto.TraerProducto(IdUsuario);
+
+            SqlConnectionStringBuilder conecctionbuilder = new();
+            conecctionbuilder.DataSource = "DESKTOP-KU34FRJ\\MSSSQLSERVER";
+            conecctionbuilder.InitialCatalog = "SistemaGestion";
+            conecctionbuilder.IntegratedSecurity = true;
+
+            var cs = conecctionbuilder.ConnectionString;
+
+
+            foreach(Producto prod in ListaProducto)
+            {
+                using (SqlConnection connection = new SqlConnection(cs))
+                {
+                    connection.Open();
+
+                    SqlCommand cmd = connection.CreateCommand();
+                    cmd.CommandText = "SELECT * FROM ProductoVendido WHERE IdProducto = @idProducto";
+
+                    var param = new SqlParameter("idProducto", SqlDbType.Int);
+                    param.Value = prod.Id;
+
+                    cmd.Parameters.Add(param);
+
+                    var reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        ProductoVendido ProductoVendido = new ProductoVendido();
+
+                        ProductoVendido.Id = Convert.ToInt32(reader.GetValue(0));
+                        ProductoVendido.Stock = Convert.ToInt32(reader.GetValue(1));
+                        ProductoVendido.IdProducto = Convert.ToInt32(reader.GetValue(2));
+                        ProductoVendido.IdVenta = Convert.ToInt32(reader.GetValue(3));
+
+
+                        listaProductosVendidos.Add(ProductoVendido);
+
+                    }
+
+                    reader.Close();
+  
+                }
+            }
+
+            return listaProductosVendidos;
+
+        }
+
+        public List<ProductoVendido> TraerProductoVendidoParaVentas(int IdVenta)
+        {
+            List<ProductoVendido> listaProductosVendidos = new List<ProductoVendido>();
+
+            SqlConnectionStringBuilder conecctionbuilder = new();
+            conecctionbuilder.DataSource = "DESKTOP-KU34FRJ\\MSSSQLSERVER";
+            conecctionbuilder.InitialCatalog = "SistemaGestion";
+            conecctionbuilder.IntegratedSecurity = true;
+
+            var cs = conecctionbuilder.ConnectionString;
+
+            using (SqlConnection connection = new SqlConnection(cs))
+            {
+                connection.Open();
+
+                SqlCommand cmd = connection.CreateCommand();
+                cmd.CommandText = "SELECT * FROM ProductoVendido WHERE IdVenta = @idVenta";
+
+                var param = new SqlParameter("idVenta", SqlDbType.Int);
+                param.Value = IdVenta;
+
+                cmd.Parameters.Add(param);
+
+                var reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    ProductoVendido ProductoVendido = new ProductoVendido();
+
+                    ProductoVendido.Id = Convert.ToInt32(reader.GetValue(0));
+                    ProductoVendido.Stock = Convert.ToInt32(reader.GetValue(1));
+                    ProductoVendido.IdProducto = Convert.ToInt32(reader.GetValue(2));
+                    ProductoVendido.IdVenta = Convert.ToInt32(reader.GetValue(3));
+
+
+                    listaProductosVendidos.Add(ProductoVendido);
+
+                }
+
+                reader.Close();
+
+                return listaProductosVendidos;
+            }
+        }
+
+        public void EliminarProductoVendido(int IdProducto)
+        {
+            SqlConnectionStringBuilder conecctionbuilder = new();
+            conecctionbuilder.DataSource = "DESKTOP-KU34FRJ\\MSSSQLSERVER";
+            conecctionbuilder.InitialCatalog = "SistemaGestion";
+            conecctionbuilder.IntegratedSecurity = true;
+
+            var cs = conecctionbuilder.ConnectionString;
+
+            using (SqlConnection connection = new SqlConnection(cs))
+            {
+                connection.Open();
+
+                SqlCommand cmd = connection.CreateCommand();
+                cmd.CommandText = "DELETE FROM ProductoVendido WHERE IdProducto = @idProducto";
+
+                var param = new SqlParameter("idProducto", SqlDbType.Int);
+                param.Value = IdProducto;
+
+                cmd.Parameters.Add(param);
+
+                cmd.ExecuteNonQuery();
+
+                connection.Close();
+
+            }
+        }
+
+        public void CrearProductoVendido(int IdProducto, int IdVenta, int Stock)
+        {
+            SqlConnectionStringBuilder conecctionbuilder = new();
+            conecctionbuilder.DataSource = "DESKTOP-KU34FRJ\\MSSSQLSERVER";
+            conecctionbuilder.InitialCatalog = "SistemaGestion";
+            conecctionbuilder.IntegratedSecurity = true;
+
+            var cs = conecctionbuilder.ConnectionString;
+
+            
+                using (SqlConnection connection = new SqlConnection(cs))
+                {
+                    connection.Open();
+
+                    SqlCommand cmd = connection.CreateCommand();
+                    cmd.CommandText = "INSERT INTO ProductoVendido(Stock, IdProducto, IdVenta) VALUES( @stock, @idProducto, @idVenta)";
+
+                    var param1 = new SqlParameter("stock", SqlDbType.Int);
+                    param1.Value = Stock;
+
+                    var param2 = new SqlParameter("idProducto", SqlDbType.Int);
+                    param2.Value = IdProducto;
+
+                    var param3 = new SqlParameter("idVenta", SqlDbType.Int);
+                    param3.Value = IdVenta;
+
+                    cmd.Parameters.Add(param1);
+                    cmd.Parameters.Add(param2);
+                    cmd.Parameters.Add(param3);
+
+                    cmd.ExecuteNonQuery();
+
+                    connection.Close();
+
+                }
+
+
+                  ProductoRepository  productos = new ProductoRepository();
+
+                productos.ModificarStockDelProducto(Stock, IdProducto);
+                    
+            
+
+        }
+    }
+}
